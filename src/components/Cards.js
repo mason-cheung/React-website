5c0d41c1b9bbfb9ff3374eb3b955a602609c8e2d
Mason <50435735+masoncch@users.noreply.github.com>
2021-02-03 16:22:21 +0800

    finishing of the website design with responsive css style

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -24,19 +24,19 @@ function Cards() {
           </ul>
           <ul className="cards__items">
               <CardItem
-              src="images/img-9.jpg"
+              src="images/img-3.jpg"
               text="Explore the hidden waterfall deep inside the Amazon Jungle"
               label="Adventure"
               path="/services"
               />
               <CardItem
-              src="images/img-2.jpg"
+              src="images/img-4.jpg"
               text="Travel through the Islands of Bali in a Private Cruise"
               label="Luxury"
               path="/services"
               />
               <CardItem
-              src="images/img-2.jpg"
+              src="images/img-8.jpg"
               text="Travel through the Islands of Bali in a Private Cruise"
               label="Luxury"
               path="/services"
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -40,13 +40,13 @@ function Navbar() {
                             </Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
-                                About
+                            <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
+                                Services
                             </Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to="/contact" className="nav-links" onClick={closeMobileMenu}>
-                                Contact
+                            <Link to="/products" className="nav-links" onClick={closeMobileMenu}>
+                                Products
                             </Link>
                         </li>
                         <li className='nav-item'>
